19cbfcf97936311067ef83bf6b8d62b6fa9a0cd4
cchalamon <cyril@410-gone.fr>
2024-01-21 16:52:13 +0100

    transformThisCartId

--- controllers/admin/AdminEverPsQuotationController.php
+++ controllers/admin/AdminEverPsQuotationController.php
@@ -207,6 +207,9 @@ class AdminEverPsQuotationController extends ModuleAdminController
 
     public function postProcess()
     {
+        if (Tools::getValue('transformThisCartId')) {
+            $this->processTransformCartToQuote();
+        }
         if (Tools::isSubmit('vieweverpsquotation_quotes')) {
             $id_everpsquotation_quotes = Tools::getValue(
                 'id_everpsquotation_quotes'
@@ -260,9 +263,9 @@ class AdminEverPsQuotationController extends ModuleAdminController
                 );
             }
             $link = new Link();
-            $createOrderLink = $link->getAdminLink('AdminOrders', true, [], ['id_cart' => $cart->id, 'addorder' => 1]);
-            Tools::redirect(
-                'http://localhost/ps8/admin7634ugpd7fexth1thbe/index.php/sell/orders/new?cartId=' . (int) $cart->id . '&_token=on87aET4vTNr7B37xQqNFiydBGBOIb-dO67MxgVoKRw'
+            $createOrderLink = $link->getAdminLink('AdminOrders', true, [], ['cartId' => $cart->id, 'addorder' => 1]);
+            Tools::redirectAdmin(
+                $createOrderLink
             );
         }
         return parent::postProcess();
@@ -308,5 +311,88 @@ class AdminEverPsQuotationController extends ModuleAdminController
         return $this->context->smarty->fetch(
             _PS_MODULE_DIR_.'everpsquotation/views/templates/admin/helpers/lists/list_action_convert_quote_to_order.tpl'
         );
+    }
+
+    protected function processTransformCartToQuote()
+    {
+        $cart = new Cart(
+            (int) Tools::getValue('transformThisCartId')
+        );
+        Hook::exec('actionBeforeCreateEverQuote');
+        $id_quote_cart = EverpsquotationCart::copyCartToQuoteCart(
+            (int)$cart->id
+        );
+        if (!Validate::isInt($id_quote_cart)) {
+            die($this->trans('An error has occured.', array(), 'Modules.Everpsquotation.Shop'));
+        }
+
+        //Create new quotation object based on current cart
+        $cartdetails = $cart->getSummaryDetails();
+        $cartproducts = $cart->getProducts();
+        if (count($cartproducts) <= 0) {
+            return;
+        }
+        $quote = new EverpsquotationClass();
+        $quote->reference = (string)Configuration::get('EVERPSQUOTATION_PREFIX');
+        $quote->id_shop_group = (int)$cart->id_shop_group;
+        $quote->id_shop = (int)$cart->id_shop;
+        $quote->id_carrier = (int)$cart->id_carrier;
+        $quote->id_lang = (int)$cart->id_lang;
+        $quote->id_customer = (int)$cart->id_customer;
+        $quote->id_cart = (int)$cart->id;
+        $quote->id_currency = (int)$cart->id_currency;
+        $quote->id_address_delivery = (int)$cart->id_address_delivery;
+        $quote->id_address_invoice = (int)$cart->id_address_invoice;
+        $quote->secure_key = (string)$cart->secure_key;
+        $quote->recyclable = (int)$cart->recyclable;
+        $quote->total_discounts = (float)$cartdetails['total_discounts'];
+        $quote->total_discounts_tax_incl = (float)$cartdetails['total_discounts'];
+        $quote->total_discounts_tax_excl = (float)$cartdetails['total_discounts_tax_exc'];
+        $quote->total_paid_tax_incl = (float)$cartdetails['total_price'];
+        $quote->total_paid_tax_excl = (float)$cartdetails['total_price_without_tax'];
+        $quote->total_products = (float)$cartdetails['total_products'];
+        $quote->total_products_wt = (float)$cartdetails['total_products_wt'];
+        $quote->total_shipping = (float)$cartdetails['total_shipping'];
+        $quote->total_shipping_tax_incl = (float)$cartdetails['total_shipping'];
+        $quote->total_shipping_tax_excl = (float)$cartdetails['total_shipping_tax_exc'];
+        $quote->total_wrapping = (float)$cartdetails['total_wrapping'];
+        $quote->total_wrapping_tax_incl = (float)$cartdetails['total_wrapping'];
+        $quote->total_wrapping_tax_excl = (float)$cartdetails['total_wrapping_tax_exc'];
+        $quote->valid = 0;
+        $quote->date_add = date('Y-m-d H:i:s');
+        $quote->date_upd = date('Y-m-d H:i:s');
+        $quote->save();
+
+        //Now create new Everpsquotationdetail object
+        foreach ($cartproducts as $cartproduct) {
+            $quotedetail = new EverpsquotationDetail();
+            $quotedetail->id_everpsquotation_quotes = (int)$quote->id;
+            $quotedetail->id_warehouse = $cartdetails['total_discounts']['id_warehouse'];
+            $quotedetail->id_shop = (int)$cartproduct['id_shop'];
+            $quotedetail->product_id = (int)$cartproduct['id_product'];
+            $quotedetail->product_attribute_id = (int)$cartproduct['id_product_attribute'];
+            $quotedetail->id_customization = (int)$cartproduct['id_customization'];
+            $quotedetail->product_name = (string)$cartproduct['name'];
+            $quotedetail->product_quantity = (int)$cartproduct['cart_quantity'];
+            $quotedetail->product_quantity_in_stock = (int)$cartproduct['stock_quantity'];
+            $quotedetail->product_price = $cartproduct['price'];
+            $quotedetail->product_ean13 = (string)$cartproduct['ean13'];
+            $quotedetail->product_isbn = (string)$cartproduct['isbn'];
+            $quotedetail->product_upc = (string)$cartproduct['upc'];
+            $quotedetail->product_reference = (string)$cartproduct['reference'];
+            $quotedetail->product_supplier_reference = (string)$cartproduct['supplier_reference'];
+            $quotedetail->product_weight = (float)$cartproduct['weight'];
+            $quotedetail->tax_name = (string)$cartproduct['tax_name'];
+            $quotedetail->ecotax = (float)$cartproduct['ecotax'];
+            $quotedetail->unit_price_tax_excl = (float)$cartproduct['price'];
+            $quotedetail->total_price_tax_incl = (float)$cartproduct['total_wt'];
+            $quotedetail->total_price_tax_excl = (float)$cartproduct['total'];
+            $quotedetail->add();
+        }
+        Hook::exec('actionAfterCreateEverQuote');
+        if ((bool)Configuration::get('EVERPSQUOTATION_RENDER_ON_VALIDATION') === true) {
+            $pdf = new PDF($quote->id, 'EverQuotationPdf', Context::getContext()->smarty);
+            $pdf->render();
+        }
     }
 }
